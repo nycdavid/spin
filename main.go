256a1fe49e21de22aa40dc8174bf641a377b0492
David Ko <davidsj.ko@gmail.com>
2022-11-22 17:37:36 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				log.Println("event:", event)
+			}
+		}
+	}()
+
+	err = watcher.Add("tmp/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-make(chan struct{})
+}
